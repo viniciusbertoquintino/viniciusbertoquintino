38f7933f9254408f341d10f0fbabbd84fce862d5
Vinicius <viniciusbertoquintino@gmail.com>
2025-10-16 18:37:32 -0300

    Refactor: update memory management and agent configuration
    Replaced the memory management and database components in `31_memory.py` to utilize `MemoryManager` and `SqliteDb`. Updated the agent model ID and improved instructions for better context handling. Transitioned from using `Playground` to `AgentOS` for agent management and serving the application.

--- estudo/31_memory.py
+++ estudo/31_memory.py
@@ -1,35 +1,37 @@
 from agno.agent import Agent
 from agno.tools.tavily import TavilyTools
 from agno.models.openai import OpenAIChat
-from agno.playground import Playground, serve_playground_app
-
-from agno.memory.v2.memory import Memory
-from agno.memory.v2.db.sqlite import SqliteMemoryDb
 
 
-memory = Memory(
-    model=OpenAIChat(id="gpt-4.1-mini"),
-    db=SqliteMemoryDb(table_name="user_memories", db_file="tmp/agent.db"),
+from agno.memory import MemoryManager #from agno.memory.v2.memory import Memory
+from agno.db.sqlite import SqliteDb #from agno.memory.v2.db.sqlite import SqliteMemoryDb
+
+from agno.os import AgentOS #from agno.playground import Playground, serve_playground_app
+
+# Configuração da memória
+memory = MemoryManager(
+    model=OpenAIChat(id="gpt-4o-mini"),
+    db=SqliteDb(table_name="user_memories", db_file="tmp/agent.db"),
 )
 
-
-
+# Criação do agente com memória
 agent = Agent(
-    model=OpenAIChat(id="gpt-4.1-mini"),
+    model=OpenAIChat(id="gpt-4o-mini"),
     tools=[TavilyTools()],
-    instructions="Você é um pesquisador. Responda sempre chamando o usuário de senhor.",
+    instructions="Você é um pesquisador especializado. Responda sempre chamando o usuário de senhor", #e use a memória para manter contexto das conversas anteriores.
     memory=memory,
     enable_agentic_memory=True,
-    # debug_mode=True
+    debug_mode=False  # Desabilitado para produção
 )
 
+agent_os = AgentOS(
+    id="my-first-os",
+    description="My first AgentOS",
+    agents=[agent],
+)
 
-
-app = Playground(agents=[
-        agent
-]).get_app()
-
+app = agent_os.get_app()
 
 if __name__ == "__main__":
-    serve_playground_app("31_memory:app", reload=True)
-
+    # Default port is 7777; change with port=...
+    agent_os.serve(app="my_os:app", reload=True)
