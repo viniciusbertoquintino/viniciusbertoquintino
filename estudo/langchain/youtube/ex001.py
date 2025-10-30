62232bb891111bf6f3eb601eb39977ef042e2166
KPMG - VIBE <vquintino@kpmg.com.br>
2025-10-30 09:00:00 -0300

    .

--- estudo/langchain/youtube/ex001.py
+++ estudo/langchain/youtube/ex001.py
@@ -8,5 +8,10 @@ from rich import print
 llm = init_chat_model("groq:llama-3.1-8b-instant")
 #llm = init_chat_model("google_genai:gemini-2.5-flash")
 
+system_message = SystemMessage(
+
+    content
+)
+
 response = llm.invoke("Olá, como vai?")
 print(response)
