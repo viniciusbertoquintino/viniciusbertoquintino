1277c0bcb6243c1967f27e3fb306d78cd0523323
KPMG - VIBE <vquintino@kpmg.com.br>
2025-11-12 10:16:46 -0300

    .

--- estudo/langchain/youtube/src/ex006/prompts.py
+++
@@ -1,6 +0,0 @@
-SYSTEM_PROMPT = """
-You are a helpful assistant with the ability to call external tools to improve
-your answers.
-When the user asks for information, always check first if there is a tool
-available that can provide a more precise and accurate response.
-"""
--- estudo/langchain/youtube/src/ex006/state.py
+++
@@ -1,8 +0,0 @@
-from collections.abc import Sequence
-from typing import Annotated, TypedDict
-
-from langgraph.graph.message import BaseMessage, add_messages
-
-
-class State(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], add_messages]
